06f4af7083e4e011f3cb47f74056cf5aaee2d47d
Maria Paktiti <maria.paktiti@gmail.com>
2024-03-27 20:26:34 -0600

    feat: (rugit diff) group changes into hunks

--- lib/diff.rb
+++ lib/diff.rb
@@ -1,3 +1,4 @@
+require_relative "./diff/hunk"
 require_relative "./diff/myers"
 
 module Diff
@@ -7,17 +8,25 @@ module Diff
         :del => "-"
     }
 
-    Edit = Struct.new(:type, :text) do
+    Line = Struct.new(:number, :text)
+
+    Edit = Struct.new(:type, :a_line, :b_line) do
         def to_s
-            SYMBOLS.fetch(type) + text
+            line = a_line || b_line
+            SYMBOLS.fetch(type) + line.text
         end
     end
 
     def self.lines(document)
-        document.is_a?(String) ? document.lines : document
+        document = document.lines if document.is_a?(String)
+        document.map.with_index { |text, i| Line.new(i + 1, text) }
     end
 
     def self.diff(a, b)
         Myers.diff(Diff.lines(a), Diff.lines(b))
+    end
+
+    def self.diff_hunks(a, b)
+        Hunk.filter(Diff.diff(a, b))
     end
 end
